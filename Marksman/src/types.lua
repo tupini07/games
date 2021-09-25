b4e8832c00b6e884a5cb56346cfde229c08968d2
Andrea Tupini <tupini07@gmail.com>
2021-09-24 18:22:10 -0600

    move types

--- Marksman/src/types.lua
+++ Marksman/src/types.lua
@@ -1,4 +1,4 @@
---- @class Bullseye:table
+--- @class Bullseye
 --- @field public x number x coordinates of top left
 --- @field public y number y coordinates of top left
 --- @field public sprite_x number x coords of sprite
@@ -7,25 +7,25 @@
 --- @field public hitbox_y number y coords of hitbox
 --- @field public hitbox_w number width of hitbox
 --- @field public hitbox_h number height of hitbox
---- @class Vector:table
+--- @class Vector
 --- @field x number
 --- @field y number
---- @class BoxCollider:table
+--- @class BoxCollider
 --- @field public x number
 --- @field public y number
 --- @field public w number
 --- @field public h number
---- @class PhysicsBody:table
+--- @class PhysicsBody
 --- @field public x number collider box x
 --- @field public y number collider box y
 --- @field public dx number
 --- @field public dy number
---- @class BoxPhysicsBody:PhysicsBody
---- @field public collider BoxCollider
---- @class Arrow:BoxPhysicsBody
+--- @class BoxPhysicsBody : PhysicsBody
+--- @field public collider BoxCollider the box collider
+--- @class Arrow : BoxPhysicsBody
 --- @field public lifetime number
 --- @field public is_stuck boolean
---- @class Spring:table
+--- @class Spring
 --- @field public x number
 --- @field public y number
 --- @field public state number 0, 1, or 2 - from less to more expanded
