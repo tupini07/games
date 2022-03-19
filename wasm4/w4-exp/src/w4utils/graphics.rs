95bc5c96e60a809c6fb4de2e54c03ce54f56aa53
Andrea Tupini <tupini07@gmail.com>
2022-03-19 09:45:55 -0600

    method to clear screen

--- wasm4/w4-exp/src/w4utils/graphics.rs
+++ wasm4/w4-exp/src/w4utils/graphics.rs
@@ -7,21 +7,47 @@ pub enum DrawColors {
     Color4,
 }
 
+impl From<u16> for DrawColors {
+    fn from(item: u16) -> Self {
+        match item {
+            1 => DrawColors::Color1,
+            2 => DrawColors::Color2,
+            3 => DrawColors::Color3,
+            4 => DrawColors::Color4,
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl From<DrawColors> for u16 {
+    fn from(item: DrawColors) -> Self {
+        match item {
+            DrawColors::Color1 => 1,
+            DrawColors::Color2 => 2,
+            DrawColors::Color3 => 3,
+            DrawColors::Color4 => 4,
+        }
+    }
+}
+
 pub fn set_palette(palette: [u32; 4]) {
     unsafe {
         *PALETTE = palette;
     }
 }
 
-pub fn set_draw_color(color: DrawColors) {
-    let c = match color {
-        DrawColors::Color1 => 1,
-        DrawColors::Color2 => 2,
-        DrawColors::Color3 => 3,
-        DrawColors::Color4 => 4,
-    };
+pub fn clear_screen(color: DrawColors) {
+    let color_num_u16: u16 = color.into();
+    let color_num: u8 = color_num_u16.try_into().unwrap();
 
-    set_draw_color_raw(c);
+    unsafe {
+        (&mut *FRAMEBUFFER)
+            .fill(color_num | (color_num << 2) | (color_num << 4) | (color_num << 6));
+    }
+}
+
+pub fn set_draw_color(color: DrawColors) {
+    set_draw_color_raw(color.into());
 }
 
 pub fn set_draw_color_raw(color: u16) {
