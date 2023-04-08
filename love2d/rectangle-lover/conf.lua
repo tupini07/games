9a2e2b0a9ea200b49aab39b1a7f5882a6b7abba5
Andrea Tupini <tupini07@gmail.com>
2023-04-07 19:48:16 -0600

    make resizable

--- love2d/rectangle-lover/conf.lua
+++ love2d/rectangle-lover/conf.lua
@@ -2,11 +2,15 @@ love.conf = function(t)
    t.gammacorrect = true
    t.title = "Rectangle Lover"
    t.identity = "rectangle-lover-1"
+   
    t.modules.joystick = false
    t.modules.physics = false
+   
    t.window.width = 720
    t.window.height = 450
    t.window.vsync = false
+   t.window.resizable = true
+
    t.version = "11.4"
    t.console = true
 end
