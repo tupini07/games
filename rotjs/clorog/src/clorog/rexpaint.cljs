3b21fadc7eb83f75d91b54fd0aa909a654bae8c9
Andrea Tupini <tupini07@gmail.com>
2023-04-13 17:48:19 -0600

    got rexparsing working

--- rotjs/clorog/src/clorog/rexpaint.cljs
+++ rotjs/clorog/src/clorog/rexpaint.cljs
@@ -1,13 +1,40 @@
 (ns clorog.rexpaint)
 
+
 (defn parse-rexpaint [rex-xml]
   (let [parser (js/DOMParser.)
         doc (.parseFromString parser rex-xml "text/xml")
-        get-inner-fn (fn [tag-name]
-                       (->> (.getElementsByTagName doc tag-name)
-                            (map #(.-innerHTML %))))
-        width (first (get-inner-fn "width"))
-        height (first (get-inner-fn "height"))]
-    (println "Parsed width" width height))
+        get-inner-fn (fn [elem tag-name]
+                       (-> (.getElementsByTagName elem tag-name)
+                           (aget 0)
+                           (.-innerHTML)))
+        width  (get-inner-fn doc "width")
+        height (get-inner-fn doc "height")
+        extract-cells-fn (fn [row] (.getElementsByTagName row "cell"))
+        extract-cell-properties (fn [cell]
+                                  {:char (char (-> (get-inner-fn cell "ascii")
+                                                   (js/parseInt)))
+                                   :fg (get-inner-fn cell "fgd")
+                                   :bg (get-inner-fn cell "bkg")})
 
-  1)
+        rows (.getElementsByTagName doc "row")]
+    (println "Parsed width" width height)
+    (->> rows
+         (map-indexed (fn [row-i row]
+                        (let [cells (extract-cells-fn row)]
+                          (->> cells
+                               (map-indexed (fn [cell-i cell]
+                                              (let [cell-properties (extract-cell-properties cell)]
+                                                (conj {:row row-i :col cell-i}
+                                                      cell-properties))))))))
+         flatten)))
+
+
+(comment
+  ;; download rexpaint xml and try parsing
+  (def a (atom nil))
+  (-> (js/fetch "/assets/sfewr.xml")
+      (.then (fn [response]
+               (.text response)))
+      (.then #(reset! a (parse-rexpaint %)))))
+
